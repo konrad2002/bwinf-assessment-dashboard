2b28efc1621bde9a9524bb280de721c6b8710f5e
Paul Franosch <paul@franos.ch>
2025-11-28 23:18:18 +0100

    stuff

--- server/src/main/java/net/myplayplanet/bwinfbackend/model/SubmissionGrading.java
+++ server/src/main/java/net/myplayplanet/bwinfbackend/model/SubmissionGrading.java
@@ -1,7 +1,16 @@
 package net.myplayplanet.bwinfbackend.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 public class SubmissionGrading {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
 }
