4ff6eefb5bc527e6c7cf9e651ac6acdb3c523c6e
Paul Franosch <paul@franos.ch>
2025-12-02 16:07:13 +0100

    stuff

--- server/src/main/java/net/myplayplanet/bwinfbackend/controller/EventController.java
+++ server/src/main/java/net/myplayplanet/bwinfbackend/controller/EventController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/api/event")
@@ -20,7 +23,12 @@ public class EventController {
     // SSE endpoint for frontend to subscribe
     @GetMapping(value = "/sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<NewEvaluationEventDto> streamEvents() {
-        return this.eventService.getSink().asFlux();
+        return eventService.getSink()
+                .asFlux()
+                .concatMap(event ->
+                        Mono.just(event)
+                                .delayElement(Duration.ofMillis((int) (Math.random() * (90000.0) + 1000.0))) // delay between events
+                );
     }
 
 
